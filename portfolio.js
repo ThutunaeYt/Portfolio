37c7eb7f334c36b52f9fb10cf54c5a98f8584259
Aryan Baral <baral.aaryan31@gmail.com>
2025-07-24 16:51:24 +0545

    Added all the files

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+// const faIcon = document.getElementsByClassName(".buttonn");
+
+// faIcon.addEventListener("click",()=>{
+//     alert("Icon Clicked");
+// })
+
+function myFunction() {
+  document.getElementById("buttonn").innerHTML = "Paragraph changed.";
+}
